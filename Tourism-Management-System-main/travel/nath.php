5f6111df852faf254a68d244d87142039f0d76d5
gisubiz-souvenir <souvenirgisubizo2004@gmail.com@.com>
2024-11-22 16:51:17 +0200

    first commit

---
+++ Tourism-Management-System-main/travel/nath.php
@@ -0,0 +1,14 @@
+<?php
+echo"<table border=1, cellpadding=5>";
+echo "<th>2</th>";
+echo "<th>3</th>";
+echo "<th>4</th>";
+echo "<th>5</th>";
+echo "<th>6</th>";
+echo "<th>7</th>";
+echo "<th>8</th>";
+echo "<th>9</th>";
+echo "<th>10</th>";
+echo "<th>11</th>";
+echo "<th>12</th>";
+?>
